82dd7797a21025e0b38f712189850b8180af052d
Hara Red <rtc6fg4.fejg2@gmail.com>
2021-12-27 19:27:24 +0300

    Add comments to Lamp

--- prefab/lamp/Lamp.cs
+++ prefab/lamp/Lamp.cs
@@ -1,18 +1,19 @@
 using Godot;
-using System;
 
 [Tool]
 public class Lamp : Spatial
 {
-    [Export]
-    public float q1;
+    [Export] // Export this field to the editor Inspector. 
+    public float q1; // You'll need more generalized coordinates.
 
     [Export]
-    public NodePath columnPath;
+    public NodePath columnPath; // You'll need paths to each controlled part.
 
     public override void _Process(float delta)
     {
+        // Extract column spatial based on the path.
         Spatial column = GetNode<Spatial>(columnPath);
+        // Set it's rotation degrees (Y, X, Z order).
         column.RotationDegrees = new Vector3(0, q1, 0);
     }
 }
